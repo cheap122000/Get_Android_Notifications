33358f8ad32af9abf20c921a4f36c0998e914bde
Tatung-Kai <weikai.chen@tatung.com>
2022-09-12 13:33:19 +0800

    fix dictionary cannot find key

--- get_adb_notifications.py
+++ get_adb_notifications.py
@@ -40,19 +40,18 @@ class Android_Notifications:
         return self.notifications
 
     def get_sms(self):
-        self.get_notifications()
-        return self.notifications["com.google.android.apps.messaging"]
+        return self.get_notification_by_pakage_name("com.google.android.apps.messaging")
 
     def get_notification_by_pakage_name(self, pkg_name: str):
         self.get_notifications()
-        return self.notifications[pkg_name]
+        return self.notifications.get(pkg_name) if self.notifications.get(pkg_name) else []
 
 if __name__ == "__main__":
     notifications = Android_Notifications()
     # print(notifications.get_notifications())
     smss = notifications.get_sms()
     test = notifications.get_notification_by_pakage_name("com.facebook.orca")
-    # print(smss)
-    # print(test)
+    print(smss)
+    print(test)
     import json
     print(json.dumps(notifications.get_notifications(), ensure_ascii=False, indent=4))
